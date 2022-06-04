673d689b8d2182ad0dc835fd1d00318a988d5ac7
darsruiz <dars2@live.com>
2022-06-04 07:02:53 +0200

    font configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,15 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    fontFamily: {
+      'CyGroteskGrandDemi': '"CyGroteskGrandDemi"',
+      'CyGroteskWideDemi': '"CyGroteskWideDemi"',
+      'GroteskWideRegular': '"GroteskWideRegular"'
+    },
     extend: {},
   },
   plugins: [
     require('tailwindcss-aria-attributes'),
+    require('@tailwindcss/typography')
   ],
 }
