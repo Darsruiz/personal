4462f25cb44ece6b87df0a650d2c831a22a6ed5f
darsruiz <dars2@live.com>
2022-06-04 07:04:27 +0200

    html changes, imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
+import { RepoCardComponent } from './shared/repo-card/repo-card.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    RepoCardComponent,
+    FooterComponent,
+    HeaderComponent,
+    ContactComponent,
+    AboutMeComponent,
+    PortfolioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
