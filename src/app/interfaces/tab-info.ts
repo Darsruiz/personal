df07a34400664ea3033030cdb9fd0c2661998ed7
darsruiz <dars2@live.com>
2022-06-04 07:03:53 +0200

    interfaces

---
+++ src/app/interfaces/tab-info.ts
@@ -0,0 +1,5 @@
+export interface ITabInfo {
+    id: number;
+    title: string;
+    description: string;
+}
---
+++ src/environments/api_variables.ts
@@ -0,0 +1,4 @@
+export const API = {
+    repos: 'https://api.github.com/users/darsruiz/repos',
+    tabInfo: '/assets/tab-info.json'
+};
